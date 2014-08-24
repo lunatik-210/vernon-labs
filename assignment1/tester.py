aa7e11ef6382cf3a368a93fa54e86957a242808d
Andrew L <andrew.d.lapin@gmail.com>
2014-08-24 17:04:57 +0400

    Added compilation to tester

--- assignment1/tester.py
+++ assignment1/tester.py
@@ -9,8 +9,10 @@ def get_file_names(tests):
 if __name__ == "__main__":
     files = get_file_names(tests)
 
+    subprocess.call("g++ main.cpp", shell=True)
+
     for file in files:
         ret = subprocess.call("./a.out " + file['input'] + " > output", shell=True)
         print file['input'], filecmp.cmp(file['output'], 'output')
 
-    ret = subprocess.call("rm output", shell=True)
+    subprocess.call("rm output a.out", shell=True)
