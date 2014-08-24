aba8396c78a64ae84fab6837a64e8bdcf1b8419b
Andrew L <andrew.d.lapin@gmail.com>
2014-08-24 17:01:41 +0400

    Added test cases

---
+++ assignment1/tester.py
@@ -0,0 +1,14 @@
+import filecmp
+import subprocess
+
+tests = ["test1", "test2"]
+
+def get_file_names(tests):
+    return [ dict(input=t+'_input', output= t+'_output') for t in tests]
+
+if __name__ == "__main__":
+    files = get_file_names(tests)
+
+    for file in files:
+        ret = subprocess.call("./a.out " + file['input'] + " > output", shell=True)
+        print file['input'], filecmp.cmp(file['output'], 'output')
